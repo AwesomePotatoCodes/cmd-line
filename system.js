ab868d1f49ee20d045f8e7935a6c7cfc92584d45
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-17 10:58:14 -0500

    Update system.js

--- system.js
+++ system.js
@@ -1,5 +1,5 @@
 var command = 0;
-const output = document.getElementById("output")
+const output = document.getElementById("output");
 function run() {
   command = document.getElementById("line").value;
   if (command == "help") {
