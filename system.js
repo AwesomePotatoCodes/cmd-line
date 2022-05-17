1b81bca14cd6c4b49395f3386c376ff70158f07a
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-17 10:56:20 -0500

    Added commands

--- system.js
+++ system.js
@@ -1,1 +1,10 @@
-
+var command = 0;
+const output = document.getElementById("output")
+function run() {
+  command = document.getElementById("line").value;
+  if (command == "help") {
+    output.innerHTML = "Commands List:<br>help : commands<br>test : testing";
+  } else if (command == "test") {
+    output.innerHTML = "Testing!";
+  }
+}
